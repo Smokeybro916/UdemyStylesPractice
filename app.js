aaee2d9e1caf7a959cc936fd3dc0a734ccfcbfba
Smokeybro916 <kyleluangasa@gmail.com>
2023-09-25 19:40:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container')
+container.style.textAlign = 'center'
+
+const image = document.querySelector('img')
+image.style.width = '150px'
+image.style.borderRadius = '50%'
+
+/* work done here in the app.js to make the text and image centered. Also changing the image style. */
